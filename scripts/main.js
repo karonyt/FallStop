9735d3d2a83575dc5eb50e6d728d0027451eb98e
かろん <104133597+karonyt@users.noreply.github.com>
2023-08-30 09:31:07 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -5,8 +5,10 @@ MC.world.afterEvents.entityHurt.subscribe((ev) => {
     const { damage, damageSource, hurtEntity } = ev;
     if (!(hurtEntity instanceof MC.Player) || damageSource.cause !== `fall`) return;
     hurtEntity.runCommandAsync(`inputpermission set @s movement disabled`)
-    hurtEntity.dimension.spawnParticle(`minecraft:death_explosion_emitter`, hurtEntity.getHeadLocation(), new MC.MolangVariableMap())
+    if(configs.showParticleOnStart) hurtEntity.dimension.spawnParticle(configs.IdOfParticleOnStart, hurtEntity.getHeadLocation(), new MC.MolangVariableMap())
+    if(configs.playSoundOnStart) hurtEntity.playSound(configs.IdOfSoundOnStart)
     MC.system.runTimeout(() => {
+        if(configs.playSoundOnStop) hurtEntity.playSound(configs.IdOfSoundOnStop)
         hurtEntity.runCommandAsync(`inputpermission set @s movement enabled`)
     }, damage * configs.stopTimeByDamageValue)
 })
