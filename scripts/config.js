b819b37112bb53f20b078fd0187b3c4386f81ba1
かろん <104133597+karonyt@users.noreply.github.com>
2023-08-30 09:31:22 +0900

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,3 +1,10 @@
 export const configs = {
     stopTimeByDamageValue: 10, //1ダメージ当たり何tick止まるか
+    playSoundOnStart: true, //麻痺開始時に音を鳴らすか
+    showParticleOnStart: true, //麻痺開始時にパーティクルを表示するか
+    playSoundOnStop: true, //麻痺終了時に音を鳴らすか
+    
+    IdOfParticleOnStart: `minecraft:death_explosion_emitter`, //麻痺開始時のパーティクル名
+    IdOfSoundOnStart: `random.glass`, //麻痺開始時に流れる音のID
+    IdOfSoundOnStop: `step.amethyst_block` //麻痺終了時に流れる音のID
 }
